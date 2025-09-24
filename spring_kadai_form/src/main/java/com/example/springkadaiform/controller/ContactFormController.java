eac35be6f3e471188d3b9c853903c73da1f81414
greentea9Q <mahoto100@yahoo.co.jp>
2025-09-24 21:40:42 +0900

    spring_kadai_formアップロード

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,33 @@
+package com.example.springkadaiform.controller;
+
+import jakarta.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springkadaiform.form.ContactForm;
+
+@Controller
+public class ContactFormController {
+
+    @GetMapping("/form")
+    public String showForm(Model model) {
+        model.addAttribute("contactForm", new ContactForm());
+        return "contactFormView";
+    }
+
+    @PostMapping("/form")
+    public String confirmForm(@Valid @ModelAttribute ContactForm contactForm,
+                              BindingResult bindingResult,
+                              Model model) {
+        if (bindingResult.hasErrors()) {
+            return "contactFormView"; // バリデーションNG → フォーム画面に戻す
+        }
+        model.addAttribute("contactForm", contactForm);
+        return "confirmView"; // バリデーションOK → 確認画面へ
+    }
+}
